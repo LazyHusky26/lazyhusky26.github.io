a1022da100a76be63ca115ea02c2c208d3349467
LazyHusky26 <aarnavthelegend26@gamil.com>
2025-06-07 11:27:47 +0530

    upd

--- p2/script.js
+++ p2/script.js
@@ -1,10 +1,14 @@
+const users = [];
+const user={}
+
 const showLogin = () => {
     let str=`
     <div class='App-container'>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <p><button onclick='Welcome()'>Log In</button></p>
+    <p><div id="dvMsg"></div></p>
+    <p><input type="text" id="txtEmail" placeholder="Enter your email"></p>
+    <p><input type="password" id="txtPass" placeholder="Enter your password"></p>
+    <p><button onclick='validateUser()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
@@ -15,22 +19,73 @@ const showRegister = () => {
     let str=`
     <div class='App-container'>
     <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <button onclick='showLogin()'>Register</button>
+    <p><input type="text" id="txtName" placeholder="Name"></p>
+     <p><input type="text" id="txtEmail" placeholder="Email"></p>
+    <p><input type="password" id="txtPass" placeholder="Password"></p>
+    <button onclick='addUser()'>Register</button>
     <p><button onclick='showLogin()'>Already have Account</button></p>
     </div>
     `
     root.innerHTML = str
 }
 
-const Welcome = () => {
+const showHome = () => {
     let str=`
     <div class='App-container'>
-    <h1>Welcome</h1>
-    <p><button onclick='showLogin()'>Logout</button></p>
+    <h1>Welcome ${user.name}</h1>
+    <hr>
+    <p><select>
+        <option value=0>--select--</option>
+        <option value=1>Deposit</option>
+        <option value=2>Withdraw</option>
+    </select></p>
+    <p><input type="number" id="txtAmount" placeholder="Enter amount"></p>
+    <p><button onclick="Transaction()">Submit</button></p>
+    <button onclick='showLogin()'>Logout</button>
+    <hr>
+    <p>Current balance: ${user.balance}<p>
     </div>
     `
     root.innerHTML = str
+}
+
+const addUser = () => {
+    const user = {
+        name: document.getElementById("txtName").value,
+        email: document.getElementById("txtEmail").value,
+        pass: document.getElementById("txtPass").value,
+        balance: 0
+    }
+    users.push(user);
+    console.log(users);
+    showLogin();
+}
+
+const validateUser = () => {
+    let email = document.getElementById("txtEmail").value;
+    let pass = document.getElementById("txtPass").value;
+    let foundUser = users.find((u) => u.email === email && u.pass === pass);
+    if (foundUser) {
+        Object.assign(user, foundUser);
+        showHome();
+    } else {
+        dvMsg.innerHTML = "Invalid email or password";
+    }
+}
+
+const Transaction = () => {
+    const type = document.querySelector("select").value;
+    const amount = parseFloat(document.getElementById("txtAmount").value);
+    if (type==1){
+        user.balance += amount;
+    }
+    else if (type==2){
+        if (user.balance >= amount){
+            user.balance -=amount;
+        }
+        else {
+            alert("Insufficient balance");
+        }
+    }
+    showHome();
 }
